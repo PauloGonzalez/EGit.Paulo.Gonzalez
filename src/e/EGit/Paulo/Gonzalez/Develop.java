ed95024d58a8ba10230dd694f0490c4d719e9bea
Raquel <Raquel@hp-ad4a5a826686>
2013-10-27 21:02:38 +0100

    CD9

--- src/e/EGit/Paulo/Gonzalez/Develop.java
+++ src/e/EGit/Paulo/Gonzalez/Develop.java
@@ -24,6 +24,12 @@ public class Develop {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 	
 	
 
