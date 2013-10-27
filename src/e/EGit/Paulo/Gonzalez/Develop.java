121125650f70ebfb87a6a18a91e22e75fa5678d8
Raquel <Raquel@hp-ad4a5a826686>
2013-10-27 21:01:39 +0100

    CD8

--- src/e/EGit/Paulo/Gonzalez/Develop.java
+++ src/e/EGit/Paulo/Gonzalez/Develop.java
@@ -18,6 +18,12 @@ public class Develop {
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 	
 	
 
